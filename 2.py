75466bd44f00512545c98708676813e724a97532
basita213 <rachid.craft2@gmail.com>
2025-04-13 13:46:17 +0200

    Add files via upload

---
+++ 2.py
@@ -0,0 +1,25 @@
+import random
+import math
+import time
+import datetime
+import os
+import sys
+import json
+import re
+import requests
+import argparse
+import csv
+import sqlite3
+import pandas 
+import numpy 
+import matplotlib.pyplot 
+import tkinter 
+import shutil
+import collections
+import itertools
+import socket
+import threading
+import multiprocessing
+
+
+print("1 + 1 = ",1+1)
